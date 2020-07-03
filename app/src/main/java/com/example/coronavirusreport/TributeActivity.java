734b52eda70085b987294d6fe9a73491bb0ea68f
ifeadikanwa <ezeifyyy@gmail.com>
2020-07-03 18:12:17 -0400

    Tribute activity added and fully functional
    created tribute activity
    changed min sdk to 17, did this to accomodate the gif dependecy I implemented
    edited 'tools:context=""' for every activity xml to point it to the right java activity
    added the candle gif

---
+++ app/src/main/java/com/example/coronavirusreport/TributeActivity.java
@@ -0,0 +1,39 @@
+package com.example.coronavirusreport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class TributeActivity extends AppCompatActivity {
+    Boolean click = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tribute);
+
+        final GifImageView candleImage = findViewById(R.id.candleImageView);
+        final Button candleButton = findViewById(R.id.candle_button);
+        final TextView candleText = findViewById(R.id.candle_textView);
+
+        candleImage.setVisibility(View.INVISIBLE);
+
+        candleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!click){
+                    click = true;
+                    candleText.setVisibility(View.INVISIBLE);
+                    candleButton.setVisibility(View.INVISIBLE);
+                    candleImage.setVisibility(View.VISIBLE);
+                }
+
+            }
+        });
+    }
+}
